5299ef98fe94feab77a0804859cc44614051a956
carlareginam10 <carlareginam10@gmail.com>
2019-05-04 15:19:58 -0300

    teste com todas as bandeiras de cartões

--- test/testCard.spec.js
+++ test/testCard.spec.js
@@ -3,16 +3,6 @@ const expect = chai.expect;
 const validator = require('../index');
 
 describe('QUANDO O CARTÃO É VALIDO', () => {
-    describe('cartão com 16 dígitos', () => {
-        it('deve retornar true', ()=>{
-            expect(validator.cardValidator('5220147622328021')).to.equal(true);                
-        });       
-    });  
-    describe('cartão visa com 16 dígitos', () => {
-        it('deve retornar true', ()=>{
-            expect(validator.cardValidator('4916726936340442')).to.equal(true);                
-        });       
-    }); 
     describe('cartão Diners com 14 dígitos', () => {
         it('deve retornar true', ()=>{
             expect(validator.cardValidator('30392141983465')).to.equal(true);                
@@ -22,8 +12,52 @@ describe('QUANDO O CARTÃO É VALIDO', () => {
         it('deve retornar true', ()=>{
             expect(validator.cardValidator('869929344656202')).to.equal(true);                
         });       
+    });
+    describe('cartão enRoute com 15 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('214913101732465')).to.equal(true);                
+        });       
+    });
+    describe('cartão American Express com 15 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('379245890734878')).to.equal(true);                
+        });       
+    }); 
+    describe('cartão Mastercard com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('5240451823631043')).to.equal(true);                
+        });       
     });  
-    
+    describe('cartão Visa com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('4916726936340442')).to.equal(true);                
+        });       
+    }); 
+    describe('cartão JCB com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('3537332718158283')).to.equal(true);                
+        });       
+    }); 
+    describe('cartão Discover com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('6011024938904087')).to.equal(true);                
+        });       
+    }); 
+    describe('cartão Elo com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('4389354672041401')).to.equal(true);                
+        });       
+    });
+    describe('cartão Hipercard com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('6062827802086544')).to.equal(true);                
+        });       
+    });
+    describe('cartão Aura com 16 dígitos', () => {
+        it('deve retornar true', ()=>{
+            expect(validator.cardValidator('5040790069663588')).to.equal(true);                
+        });       
+    });   
    
 }); 
 
@@ -36,6 +70,11 @@ describe('QUANDO O CARTÃO É INVÁLIDO', () => {
     describe('e tem menos que 16 números', () => {
         it('deve retornar false', ()=>{
             expect(validator.cardValidator('786')).to.equal(false);
+        });       
+    });
+    describe('e é vazio', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.cardValidator('')).to.equal(false);
         });       
     });
     
@@ -59,11 +98,22 @@ describe('QUANDO O CARTÃO É INVÁLIDO', () => {
             expect(validator.cardValidator("1000000000000000")).to.equal(false);
         });       
     });
-    describe('e é um composto por sequencia de zeros', () => {
+    
+    describe('e é um composto por sequencia de 8', () => {
         it('deve retornar false', ()=>{
-            expect(validator.cardValidator("7777777777777777")).to.equal(false);
+            expect(validator.cardValidator("88888888888888")).to.equal(false);
         });       
     });
+    describe('e é um boleano true', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.cardValidator(true)).to.equal(false);
+        });       
+    })
+    describe('e é um boleano false', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.cardValidator(false)).to.equal(false);
+        });       
+    })
    
 
 
